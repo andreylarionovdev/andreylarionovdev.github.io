95edea46afc045b468dd85e111ae440116b2b89c
Andrey Larionov <andrey.larionov.dev@gmail.com>
2019-09-29 01:08:18 +0200

    UI Kit -> Form components: range slider implemented

--- src/components/form/range-slider.js
+++ src/components/form/range-slider.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
-    $('.range-slider').ionRangeSlider({
+    var render = function (data) {
+        var $label  = $('.label--for-range-slider').find('.label__right');
+        var from    = data.from;
+        var to      = data.to;
+        var range   = from.toLocaleString('ru-RU') + '₽ - ' + to.toLocaleString('ru-RU') + '₽';
+
+        $label.text(range);
+    };
+
+    var $slider = $('.range-slider__input');
+    $slider.ionRangeSlider({
         type: 'double',
         skin: 'big',
         min: 0,
-        max: 15000,
+        max: 16000,
         from: 5000,
-        to: 10000
+        to: 10000,
+        hide_min_max: true,
+        hide_from_to: true,
+        onStart: render,
+        onChange: render
     });
 });
