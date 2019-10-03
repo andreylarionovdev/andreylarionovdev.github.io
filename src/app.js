4f60f4a0cfca624868f33b1800d43e38fb550122
Andrey Larionov <andrey.larionov.dev@gmail.com>
2019-10-03 09:56:07 +0200

    UI Kit -> Form components: input text and checkbox mixins and styles refactored

--- src/app.js
+++ src/app.js
@@ -3,6 +3,5 @@ import '../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js'
 import '../node_modules/paginationjs/dist/pagination.min.js'
 
 // App
-import './components/form/input-text-group.js'
 import './components/form/range-slider.js'
 import './components/form/paginator.js'
