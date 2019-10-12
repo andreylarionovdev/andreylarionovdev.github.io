6cf1f1f804ddae9885b808d7df4f1497dbefd990
Andrey Larionov <andrey.larionov.dev@gmail.com>
2019-10-13 00:59:07 +0200

    UI Kit -> Form components: responsive done.

--- src/components/form/exp-check-list/exp-check-list.js
+++ src/components/form/exp-check-list/exp-check-list.js
@@ -6,5 +6,20 @@ $(document).ready(function () {
         } else {
             $dropdown.addClass('exp-check-list--expanded');
         }
+        resize($dropdown);
+    });
+
+    var resize = function($dropdown) {
+        var $drop   = $dropdown.find('.exp-check-list__list')
+            , dropH = 0
+        ;
+        if ($dropdown.hasClass('exp-check-list--expanded')) {
+            dropH = $drop.outerHeight() + 'px';
+        }
+        $dropdown.css('margin-bottom', dropH);
+    };
+
+    $('.exp-check-list--expanded').each(function (i, o) {
+        resize($(o));
     });
 });
