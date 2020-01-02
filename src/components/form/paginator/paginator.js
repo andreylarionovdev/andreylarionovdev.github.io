54636dda1d3394dd77c2cee448cbc1ae5d5c8d4a
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-01-02 23:57:07 +0200

    paginator component refactoring

--- src/components/form/paginator/paginator.js
+++ src/components/form/paginator/paginator.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
-    var $pager = $('.paginator');
-    if ($pager.length < 1) {
-        return;
-    }
+  const className = 'paginator';
+  $(`.${className}`).each(function (i, o) {
+    const $pager = $(o);
+
+    const dataSource = $pager.data('source') || [];
+    const pageSize = $pager.data('page-size') || 12;
+    const pageRange = $pager.data('page-range') || 1;
+
     $pager.pagination({
-        dataSource: [...Array(180).keys()],
-        pageSize: 12,
-        pageRange: 1,
-        autoHidePrevious: true,
-        autoHideNext: true,
-        showNavigator: true,
-        classPrefix: 'paginator',
-        activeClassName: 'paginator-page--active',
-        disableClassName: 'paginator-page--disabled',
-        ulClassName: 'paginator__ul',
-        prevText: '<i class="material-icons paginator__arrow-prev">arrow_back</i>',
-        nextText: '<i class="material-icons paginator__arrow-next">arrow_forward</i>',
-        formatNavigator: '<%= currentPage %> &ndash; 12 из 100+ вариантов аренды'
+      dataSource: dataSource,
+      pageSize: pageSize,
+      pageRange: pageRange,
+      autoHidePrevious: true,
+      autoHideNext: true,
+      showNavigator: true,
+      classPrefix: 'paginator',
+      activeClassName: 'paginator-page--active',
+      disableClassName: 'paginator-page--disabled',
+      ulClassName: 'paginator__ul',
+      prevText: `<i class="material-icons ${className}__arrow-prev">arrow_back</i>`,
+      nextText: `<i class="material-icons ${className}__arrow-next">arrow_forward</i>`,
+      formatNavigator: `<%= currentPage %> &ndash; 12 из 100+ вариантов аренды`
     });
+  });
 });
