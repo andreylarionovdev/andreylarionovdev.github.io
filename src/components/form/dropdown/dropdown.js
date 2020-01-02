67a3dbefbf17402f59aed5db14bc61fc5862fdab
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-01-02 15:31:26 +0200

    refactoring

--- src/components/form/dropdown/dropdown.js
+++ src/components/form/dropdown/dropdown.js
@@ -1,64 +1,50 @@
 $(document).ready(function () {
-    var $select         = $('.dropdown__select')
-        , expandedClass = 'dropdown--expanded'
-        , $inc          = $('.dropdown__li-inc')
-        , $dec          = $('.dropdown__li-dec')
-    ;
-    //// By click on select ...
-    $select.on('click', function () {
-        var $dropdown = $(this).closest('.dropdown');
-        //// .. if with drop menu ..
-        if ($dropdown.find('.dropdown__menu').length === 1) {
-            //// ... toggle modifying class on parent dropdown
-            if ($dropdown.hasClass(expandedClass)) {
-                $dropdown.removeClass(expandedClass);
-            } else {
-                $dropdown.addClass(expandedClass);
-            }
-        }
-    });
+  const $select = $('.dropdown__select');
+  const classExpanded = 'dropdown--expanded';
+  const $inc = $('.dropdown__li-inc');
+  const $dec = $('.dropdown__li-dec');
 
-    // +/- click handler
-    const numberChangerOnClick = function () {
-        var $li         = $(this).closest('.dropdown__li')
-            , $dropdown = $(this).closest('.dropdown')
-            , $count    = $li.find('.dropdown__li-count')
-            , n         = parseInt($count.text())
-            , operator  = '+'
-        ;
-        if ($(this).closest('.dropdown__li-dec').length === 1) {
-            operator = '-';
-        }
-        switch (operator) {
-            case '+':
-                n++;
-                break;
-            case '-':
-                n--;
-                break;
-            default:
-                n;
-        }
-        if (n < 0 || n > 5) {
-            return;
-        }
-        $count.text(n);
-        $dropdown
-            .addClass('dropdown--edited')
-            .removeClass('dropdown--empty');
-    };
-    $inc.on('click', numberChangerOnClick);
-    $dec.on('click', numberChangerOnClick);
+  $select.on('click', function () {
+    const $dropdown = $(this).closest('.dropdown');
+    if ($dropdown.find('.dropdown__menu').length === 1) {
+      $dropdown.toggleClass(classExpanded)
+    }
+  });
 
-    $('.dropdown__li-clear').on('click', function () {
-        $(this).closest('.dropdown').find('.dropdown__li-count').each(function (i, o) {
-            $(o).text(0);
-        })
-    });
+  // +/- click handler
+  const numberChangerOnClick = function () {
+    const $li = $(this).closest('.dropdown__li');
+    const $dropdown = $(this).closest('.dropdown');
+    const $count = $li.find('.dropdown__li-count');
 
-    $('.dropdown__li-apply').on('click', function () {
-        $(this).closest('.dropdown')
-            .removeClass('dropdown--edited')
-            .removeClass('dropdown--expanded');
-    });
+    let n = parseInt($count.text())
+    let operator = $(this).closest('.dropdown__li-dec').length === 1
+      ? '-'
+      : '+';
+
+    n = operator === '+' ? n + 1 : n - 1;
+
+    if (n < 0 || n > 5) {
+      return;
+    }
+    $count.text(n);
+    $dropdown
+      .addClass('dropdown--edited')
+      .removeClass('dropdown--empty');
+  };
+
+  $inc.on('click', numberChangerOnClick);
+  $dec.on('click', numberChangerOnClick);
+
+  $('.dropdown__li-clear').on('click', function () {
+    $(this).closest('.dropdown').find('.dropdown__li-count').each(function (i, o) {
+      $(o).text(0);
+    })
+  });
+
+  $('.dropdown__li-apply').on('click', function () {
+    $(this).closest('.dropdown')
+      .removeClass('dropdown--edited')
+      .removeClass('dropdown--expanded');
+  });
 });
