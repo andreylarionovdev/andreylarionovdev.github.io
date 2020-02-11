f973b74f5f4addc9285dec00eb1cd08628046ac4
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-02-12 01:56:52 +0200

    Refactoring: a bunch of eslint fixes.

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,30 +1,33 @@
-export const bem = function ({b, e, m, js} = {}) {
-  let classes = [];
+const bem = ({
+  b, e, m, js,
+} = {}) => {
+  const classes = [];
   let cls = b;
   const isElement = b && e;
 
   if (isElement) {
-    cls = `${b}__${e}`
+    cls = `${b}__${e}`;
   }
 
   classes.push(cls);
 
   if (m) {
     if (Array.isArray(m)) {
-      m.map((m) => {
-        classes.push(`${cls}--${m}`);
-      });
+      m.map((_m) => classes.push(`${cls}--${_m}`));
     } else {
       classes.push(`${cls}--${m}`);
     }
   }
   if (js) {
-    classes.push(`${js}-${cls}`)
+    classes.push(`${js}-${cls}`);
   }
 
   return classes;
 };
 
-export const formatCurrency = function({value, locale = 'ru-RU', sign = '₽'}) {
-  return `${value.toLocaleString(locale).replace(',', '&nbsp;')}${sign}`
+const formatCurrency = ({ value, locale = 'ru-RU', sign = '₽' }) => `${value.toLocaleString(locale).replace(',', '&nbsp;')}${sign}`;
+
+export {
+  bem,
+  formatCurrency,
 };
