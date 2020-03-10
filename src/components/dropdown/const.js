381f9dd191273d98535004e30170418388c208b9
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-03-10 15:59:59 +0200

    issue #10: Dropdown 3 - 6) fixed

---
+++ src/components/dropdown/const.js
@@ -0,0 +1,9 @@
+const GUEST_TYPE = 'guest';
+const ROOM_TYPE = 'room';
+const COUNT_MIN_VALUE = 0;
+
+export {
+  GUEST_TYPE,
+  ROOM_TYPE,
+  COUNT_MIN_VALUE,
+};
--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,40 +1,87 @@
 import $ from 'jquery';
+import {
+  GUEST_TYPE,
+  ROOM_TYPE,
+  COUNT_MIN_VALUE,
+} from './const';
 
-const Dropdown = function Dropdown() {
-  this.init();
+const Dropdown = function Dropdown($element) {
+  this.init($element);
 };
 
-Dropdown.prototype.init = function init() {
+Dropdown.prototype.init = function init($element) {
+  this.$element = $element;
+
   this.classExpanded = 'dropdown_expanded';
-  this.classEdited = 'dropdown_edited';
   this.classEmpty = 'dropdown_empty';
+  this.classHiddenButtons = 'dropdown_with-hidden-buttons';
   this.classChangeCountButtonDisabled = 'dropdown__change-count-button_disabled';
 
-  this.selectorDropdown = '.js-dropdown';
-  this.selectorToggleButton = '.js-dropdown__toggle-button';
   this.selectorMenu = '.js-dropdown__menu';
   this.selectorMenuItem = '.js-dropdown__menu-item';
   this.selectorChangeCountButton = '.js-dropdown__change-count-button';
+  this.selectorDecCountButton = '.js-dropdown__change-count-button_type_dec';
   this.selectorCount = '.js-dropdown__count';
-  this.selectorClearButton = '.js-dropdown__clear-button';
-  this.selectorApplyButton = '.js-dropdown__apply-button';
 
+  this.$toggleButton = this.$element.find('.js-dropdown__toggle-button');
+  this.$selection = this.$element.find('.js-dropdown__selection');
+  this.$changeCountButton = this.$element.find(this.selectorChangeCountButton);
+  this.$clearButton = this.$element.find('.js-dropdown__clear-button');
+  this.$applyButton = this.$element.find('.js-dropdown__apply-button');
+
+  this.parseProperties();
   this.addEventListeners();
 };
 
+Dropdown.prototype.parseProperties = function parseProperties() {
+  this.type = this.$element.attr('data-type');
+
+  if ([GUEST_TYPE, ROOM_TYPE].includes(this.type)) {
+    this.initState();
+    this.updateValue();
+    this.updateView();
+  }
+};
+
+Dropdown.prototype.initState = function initType() {
+  const stateTemplates = {
+    [GUEST_TYPE]: {
+      total: 0,
+      emptyValue: 'Сколько гостей',
+      categories: [
+        { count: 0, wordForms: ['гость', ...new Array(3).fill('гостя'), 'гостей'] },
+        { count: 0, wordForms: ['гость', ...new Array(3).fill('гостя'), 'гостей'] },
+        { count: 0, wordForms: ['младенец', ...new Array(3).fill('младенца'), 'младенцев'] },
+      ],
+    },
+    [ROOM_TYPE]: {
+      total: 0,
+      emptyValue: 'Сколько комнат',
+      categories: [
+        { count: 0, wordForms: ['спальня', ...new Array(3).fill('спальни'), 'спален'] },
+        { count: 0, wordForms: ['кровать', ...new Array(3).fill('кровати'), 'кроватей'] },
+        { count: 0, wordForms: ['ванная комната', ...new Array(3).fill('ванные комнаты'), 'ванных комнат'] },
+      ],
+    },
+  };
+
+  this.state = stateTemplates[this.type];
+
+  this.updateState();
+};
+
 Dropdown.prototype.addEventListeners = function addEventListeners() {
-  $(this.selectorToggleButton).on('click', this.handleToggleButtonClick.bind(this));
-  $(this.selectorChangeCountButton).on('click', this.handleChangeCountButtonClick.bind(this));
-  $(this.selectorClearButton).on('click', this.handleClearButtonClick.bind(this));
-  $(this.selectorApplyButton).on('click', this.handleApplyButtonClick.bind(this));
+  this.$toggleButton.on('click', this.handleToggleButtonClick.bind(this));
+  this.$changeCountButton.on('click', this.handleChangeCountButtonClick.bind(this));
+  this.$clearButton.on('click', this.handleClearButtonClick.bind(this));
+  this.$applyButton.on('click', this.handleApplyButtonClick.bind(this));
 };
 
 Dropdown.prototype.handleToggleButtonClick = function handleDropdownClick(e) {
   e.preventDefault();
 
-  const $dropdown = $(e.currentTarget).closest(this.selectorDropdown);
-  if ($dropdown.find(this.selectorMenu).length === 1) {
-    $dropdown.toggleClass(this.classExpanded);
+  if (this.$element.find(this.selectorMenu).length === 1) {
+    this.$element.toggleClass(this.classExpanded);
   }
 };
 
@@ -57,31 +104,88 @@ Dropdown.prototype.handleChangeCountButtonClick = function handleChangeCountButt
   const $count = $li.find(this.selectorCount);
   const n = changeCount[operator](parseInt($count.text(), 10));
 
-  if (n < 1) {
+  if (n === COUNT_MIN_VALUE) {
     $button.addClass(this.classChangeCountButtonDisabled);
   }
 
-  if (n < 0) {
+  if (n < COUNT_MIN_VALUE) {
     return;
   }
 
   $count.text(n);
 
-  const $dropdown = $(e.currentTarget).closest(this.selectorDropdown);
-  $dropdown.addClass(this.classEdited).removeClass(this.classEmpty);
+  this.$element.removeClass(`${this.classEmpty} ${this.classHiddenButtons}`);
+
+  this.updateState();
 };
 
 Dropdown.prototype.handleClearButtonClick = function handleClearButtonClick(e) {
   e.preventDefault();
 
-  const $counts = $(e.currentTarget).closest(this.selectorDropdown).find(this.selectorCount);
+  const $counts = this.$element.find(this.selectorCount);
   $counts.each((i, o) => $(o).text(0));
+
+  const $decButtons = this.$element.find(this.selectorDecCountButton);
+  $decButtons.each((i, o) => {
+    $(o).addClass(this.classChangeCountButtonDisabled);
+  });
+
+  this.updateState();
+  this.updateView();
 };
 
 Dropdown.prototype.handleApplyButtonClick = function handleApplyButtonClick(e) {
   e.preventDefault();
 
-  $(e.currentTarget).closest(this.selectorDropdown).removeClass(`${this.classEdited} ${this.classExpanded}`);
+  this.updateValue();
+  this.$element.removeClass(this.classExpanded);
 };
 
-$(() => new Dropdown());
+Dropdown.prototype.updateState = function updateState() {
+  this.state.total = 0;
+  this.$element.find(this.selectorMenuItem).each((i, o) => {
+    const count = parseInt($(o).find(this.selectorCount).text(), 10);
+    this.state.categories[i].count = count;
+    this.state.total += count;
+  });
+};
+
+Dropdown.prototype.updateValue = function updateValue() {
+  const countsWithCategories = this.state.categories.map((category) => {
+    const { count, wordForms } = category;
+
+    if (count === 0) return '';
+
+    const wordForm = wordForms[count - 1] || wordForms[wordForms.length - 1];
+
+    return `${count} ${wordForm}`;
+  });
+
+  this.$selection.text(this.buildValue(countsWithCategories));
+};
+
+Dropdown.prototype.updateView = function updateView() {
+  if (this.state.total === 0) {
+    this.$element.addClass(this.classEmpty);
+  }
+};
+
+Dropdown.prototype.buildValue = function buildValue(countsWithCategories) {
+  if (this.state.total === 0) {
+    return this.state.emptyValue;
+  }
+
+  if (this.type === GUEST_TYPE) {
+    const { wordForms } = this.state.categories[0];
+    const wordForm = wordForms[this.state.total - 1] || wordForms[wordForms.length - 1];
+
+    const guests = `${this.state.total} ${wordForm}`;
+    const babies = countsWithCategories[countsWithCategories.length - 1];
+
+    return babies ? `${guests}, ${babies}` : guests;
+  }
+
+  return countsWithCategories.filter(Boolean).join(', ');
+};
+
+$(() => $('.js-dropdown').each((i, o) => new Dropdown($(o))));
