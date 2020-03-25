15e241af1c8fc2fe11452e6a1c811cfc9336c6bb
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-03-25 14:35:42 +0200

    issue #10: filter page - filter with js buttons; minimum 2 columns of thumnails (except xs)

--- src/components/room-filter-page/room-filter-page.js
+++ src/components/room-filter-page/room-filter-page.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 import '../../utils/app.scss';
 
 import '../dropdown/dropdown';
@@ -17,3 +19,54 @@ import './img/room-thumb-9.png';
 import './img/room-thumb-10.png';
 import './img/room-thumb-11.png';
 import './img/room-thumb-12.png';
+
+const RoomFilterPage = function RoomFilterPage() {
+  this.init();
+};
+
+RoomFilterPage.prototype.init = function init() {
+  this.classHiddenShowFilterButton = 'room-filter-page__show-filter-button_hidden';
+  this.classVisibleFilter = 'room-filter-page__form_visible';
+  this.classHiddenThumbnails = 'room-filter-page__thumbnail-list-wrapper_hidden';
+  this.classHiddenFooter = 'footer_hidden';
+
+  this.$showFilterButton = $('.js-room-filter-page__show-filter-button');
+  this.$clearFilterButton = $('.js-room-filter-page__clear-filter-button');
+  this.$applyFilterButton = $('.js-room-filter-page__apply-filter-button');
+
+  this.$filterForm = $('.js-room-filter-page__form');
+  this.$thumbnailsWrapper = $('.js-room-filter-page__thumbnail-list-wrapper');
+  this.$footer = $('.js-footer');
+
+  this.addEventListeners();
+};
+
+RoomFilterPage.prototype.addEventListeners = function addEventListeners() {
+  this.$showFilterButton.on('click', this.handleShowFilterButtonClick.bind(this));
+  this.$clearFilterButton.on('click', this.handleClearFilterButtonClick.bind(this));
+  this.$applyFilterButton.on('click', this.handleApplyFilterButtonClick.bind(this));
+};
+
+RoomFilterPage.prototype.handleShowFilterButtonClick = function handleShowFilterButtonClick(e) {
+  e.preventDefault();
+  this.toggleFilter();
+};
+
+RoomFilterPage.prototype.handleClearFilterButtonClick = function handleClearFilterButtonClick(e) {
+  e.preventDefault();
+  this.toggleFilter();
+};
+
+RoomFilterPage.prototype.handleApplyFilterButtonClick = function handleClearFilterButtonClick(e) {
+  e.preventDefault();
+  this.toggleFilter();
+};
+
+RoomFilterPage.prototype.toggleFilter = function toggleFilter() {
+  this.$showFilterButton.toggleClass(this.classHiddenShowFilterButton);
+  this.$filterForm.toggleClass(this.classVisibleFilter);
+  this.$thumbnailsWrapper.toggleClass(this.classHiddenThumbnails);
+  this.$footer.toggleClass(this.classHiddenFooter);
+};
+
+$(() => new RoomFilterPage());
