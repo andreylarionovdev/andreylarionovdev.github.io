bc983a296f050be5194d27f7ab42689b2949d9dd
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-07-15 21:43:04 +0200

    fix(tooltip): use jquery-ui tooltip
    #12

--- src/components/room-book/room-book.js
+++ src/components/room-book/room-book.js
@@ -2,7 +2,18 @@ import $ from 'jquery';
 import Dropdown from '../dropdown/dropdown';
 import Calendar from '../calendar/calendar';
 
+require('webpack-jquery-ui/tooltip');
+
 $(() => {
+  $('.js-room-book__result-grid').tooltip({
+    classes: {
+      'ui-tooltip': 'room-book__tooltip',
+    },
+    position: {
+      my: 'center bottom',
+      at: 'center top',
+    },
+  });
   // eslint-disable-next-line no-unused-vars
   const guestsDropdown = new Dropdown($('.js-room-book__dropdown .js-dropdown'), {
     type: Dropdown.prototype.guestsType,
