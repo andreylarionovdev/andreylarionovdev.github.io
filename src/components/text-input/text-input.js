405b649db6ef916b3424be05cc247763301256eb
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-09-28 10:02:15 +0200

    fix(imaskjs): fix wrong input selector
    #22

--- src/components/text-input/text-input.js
+++ src/components/text-input/text-input.js
@@ -1,6 +1,6 @@
 import IMask from 'imask';
 
-const TextInput = function TextInput($element, options= {}) {
+const TextInput = function TextInput($element, options = {}) {
   this.init($element, options);
 };
 
@@ -13,39 +13,36 @@ TextInput.prototype.init = function init($element, options) {
   }
 };
 
-TextInput.prototype.createIMaskInstance = function initIMaskInstance() {
-  this.IMaskInstnace = new IMask(
-    this.$element.find('input[type="text"]')[0],
-    {
-      mask: Date,
-      lazy: false,
-      overwrite: true,
-      autofix: true,
-      blocks: {
-        d: {
-          mask: IMask.MaskedRange,
-          placeholderChar: 'Д',
-          from: 1,
-          to: 31,
-          maxLength: 2,
-        },
-        m: {
-          mask: IMask.MaskedRange,
-          placeholderChar: 'М',
-          from: 1,
-          to: 12,
-          maxLength: 2,
-        },
-        Y: {
-          mask: IMask.MaskedRange,
-          placeholderChar: 'Г',
-          from: 1900,
-          to: 2999,
-          maxLength: 4,
-        },
+TextInput.prototype.createIMaskInstance = function createIMaskInstance() {
+  this.IMaskInstance = IMask(this.$element.find('input').get(0), {
+    mask: Date,
+    lazy: false,
+    overwrite: true,
+    autofix: true,
+    blocks: {
+      d: {
+        mask: IMask.MaskedRange,
+        placeholderChar: 'Д',
+        from: 1,
+        to: 31,
+        maxLength: 2,
+      },
+      m: {
+        mask: IMask.MaskedRange,
+        placeholderChar: 'М',
+        from: 1,
+        to: 12,
+        maxLength: 2,
+      },
+      Y: {
+        mask: IMask.MaskedRange,
+        placeholderChar: 'Г',
+        from: 1900,
+        to: 2999,
+        maxLength: 4,
       },
     },
-  );
+  });
 };
 
 export default TextInput;
